dc1a535e92ab54e471a975c9863c5322d315b5e7
Priyanka Gupta <86978649+PriyankaGeeta@users.noreply.github.com>
2022-02-04 19:19:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(process.env.PORT || 5000, () => {
+const server = app.listen(process.env.PORT || 5000, () => {
   const port = server.address().port;
-  console.log(`Express is working on port ${port}`))
+  console.log(`Express is working on port ${port}`);
+});
