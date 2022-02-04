1d4bbfe1c789c84a00144a08d3207c48ce6561b4
Priyanka Gupta <86978649+PriyankaGeeta@users.noreply.github.com>
2022-02-04 19:14:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const express = require('express')
 const app = express()
-const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.listen(process.env.PORT || 5000, () => {
+  const port = server.address().port;
+  console.log(`Express is working on port ${port}`))
